1757e6233a292edca23bd1bae692efd91c1bd517
Aldwin Lagman <lagman.aldwin@gmail.com>
2023-04-02 04:17:43 +0800

    added page factory

--- src/test/java/tests/examples/GoogleTest.java
+++ src/test/java/tests/examples/GoogleTest.java
@@ -2,21 +2,33 @@ package tests.examples;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import PageFactory.GoogleHomePage;
 import utils.BaseTest;
 import utils.Steps;
 import static utils.Driver.webDriver;
 
 public class GoogleTest extends BaseTest {
     Steps step = new Steps();
+
+    /**
+     * Example of code using page factory
+     */
     @Test
     public void whatsMyIp() {
+        
+        GoogleHomePage googleHomePage = new GoogleHomePage(webDriver);
+
         step.launchBrowser();
-        step.sendKeys(webDriver.findElement(By.xpath("//*[@name='q']")), "whats my ip");
-        step.click(webDriver.findElement(By.xpath(" (//*[@name='btnK'])[1]")));
-        step.getText(webDriver.findElement(By.xpath("//w-answer-desktop/div[1]")));
+        step.sendKeys(googleHomePage.txtboxSearch, "whats my ip");
+        step.click(googleHomePage.btnSearch);
+        step.getText(googleHomePage.labelIpAddress);
 
     }
 
+    /**
+     * Example of code using findElement
+     */
     @Test
     public void searchMyName() {
         step.launchBrowser();
